eebb3d2b9b91dc68e656adfc3142c7c9676c719e
JustinMont07 <ja28mont@siena.edu>
2024-09-25 09:33:25 -0400

    Intial Push

---
+++ components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { s } from "./Home.style";
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <View style={s.container}>
+      <Text> Home </Text>
+    </View>
+  );
+}
